3fc14a627001bf36478d5c9c6944844a19528238
David Comor <david.comor@gmail.com>
2025-10-09 09:57:25 +0000

    I'm dumb and wasnt running dbService as an instance in server.py

--- dbService.py
+++ dbService.py
@@ -13,8 +13,11 @@ import os
 
 
 class DBService:
-    def __init__(self, dsn: str):
-        self.conn = psycopg2.connect(dsn)
+    def __init__(self):
+        self.dsn = os.getenv("DATABASE_DSN")
+        atexit.register(lambda: self.conn.close())
+
+        self.conn = psycopg2.connect(self.dsn)
         self.cur = self.conn.cursor()
         self.dict_cur: psycopg2.extensions.cursor = self.conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
@@ -46,168 +49,169 @@ class DBService:
     def query_dict_data(self, sql: str, params: list[str]):
         self.dict_cur.execute(sql, params)
         return self.dict_cur.fetchall()
-
-dsn = os.getenv("DATABASE_DSN")
-serviceInstance = DBService(dsn) # type: ignore
-atexit.register(lambda: serviceInstance.conn.close())
-
-
-
-def verifyAccessKey(username: str, accessKey: str) -> str | None:
-    if (len(accessKey) != 64*2) or (not accessKey.isalnum()):
-        return None
     
-    queryResult = serviceInstance.query_data("""
-        SELECT accessKey
-        FROM users
-        WHERE (
-            users.username = %s AND
-            users.accessKey = %s
-        );""",
-        [username, accessKey]
-        )
+    def verifyAccessKey(self, username: str, accessKey: str) -> str | None:
+        if (len(accessKey) != 64*2) or (not accessKey.isalnum()):
+            return None
+        
+        queryResult = self.query_data("""
+            SELECT accessKey
+            FROM users
+            WHERE (
+                users.username = %s AND
+                users.accessKey = %s
+            );""",
+            [username, accessKey]
+            )
 
-    if (bool(queryResult)):
-        return queryResult[0][0]
-    return None
+        if (bool(queryResult)):
+            return queryResult[0][0]
+        return None
 
 
-def createUser(username: str, email: str, password: str, test:bool = False) -> int:
-    env = (utils.envs.test if test else utils.envs.prod)
-    user = User(username=username, email=email, password=password, env=env)
+    def createUser(self, username: str, email: str, password: str, test:bool = False) -> int:
+        env = (utils.envs.test if test else utils.envs.prod)
+        user = User(username=username, email=email, password=password, env=env)
 
-    if not utils.validateEmail(email):
-        raise InvalidEmail("Invalid email format.")
-    elif getUsers(username, None, None, None):
-        raise NonuniqueUsername(f"User '{username}' already exists.")
-    else:
-        serviceInstance.modify_data("""
-            INSERT INTO users (username, uniqueid, email, accessKey, password)
-            VALUES (%s, %s, %s, %s, %s);
+        if not utils.validateEmail(email):
+            raise InvalidEmail("Invalid email format.")
+        elif self.getUsers(username, None, None, None):
+            raise NonuniqueUsername(f"User '{username}' already exists.")
+        else:
+            self.modify_data("""
+                INSERT INTO users (username, uniqueid, email, accessKey, password)
+                VALUES (%s, %s, %s, %s, %s);
+                """, 
+                [user.getUsername(), user.getUniqueid(), user.getEmail(), user.getAccessKey(), user.getPassword()])
+
+            return 1
+
+
+    def getUsers(
+        self,
+        username: Optional[str],
+        uniqueid: Optional[str],
+        email: Optional[str],
+        accessKey: Optional[str]
+    ) -> list[User]:
+
+        searchStatements: list[str] = []
+        data: list[str] = []
+
+        for key, value in {**locals()}.items():
+            if (type(value) == str):
+                searchStatements.append(f"{str(key)} = %s")
+                data.append(value)
+
+        searchStatement = " AND ".join(searchStatements)
+
+        queryResult = self.query_dict_data(("SELECT * FROM users WHERE " + searchStatement), data)
+        
+        users: list[User] = []
+
+        for result in queryResult:
+            userEnv = utils.envs[str(utils.uniqueIdToMap(result['uniqueid'])['env'])] # type: ignore
+            user = User(
+                username=result['username'], # type: ignore
+                uniqueid=result['uniqueid'], # type: ignore
+                email=result['email'], # type: ignore
+                accessKey=result['accesskey'], # type: ignore
+                password=result['password'], # type: ignore
+                env=userEnv
+            )
+            users.append(user)
+
+        return users
+
+
+    def updateUser(
+        self,
+        uniqueid: str,
+        valuesToUpdate: dict[str, str]
+    ):
+        
+        allowedValues: set[str] = {"email", "accessKey", "password"}
+
+        setStatements: list[str] = []
+
+        for key, value in valuesToUpdate.items():
+            if key not in allowedValues:
+                raise KeyError(f"Invalid key '{key}' in valuesToUpdate. Allowed keys are: {allowedValues}")
+            if key == "email" and not utils.validateEmail(value):
+                raise KeyError(f"Invalid email format for value: {value}")
+            setStatements.append(str(key + " = '" + value + "'"))
+
+        setStatement: str = ", ".join(setStatements)
+        
+        self.modify_data(("UPDATE users SET " + setStatement + " WHERE users.uniqueID = %s"), [uniqueid])
+
+
+    def login(
+        self,
+        username: str,
+        password: str
+    ) -> str | None:
+        
+        queryResult = self.query_dict_data(("SELECT * FROM users WHERE username=%s"), [username])
+
+        if len(queryResult) == 1:
+            try:
+                if utils.verifyHash(queryResult[0]["password"], password): # type: ignore
+                    return queryResult[0]['accesskey'] # type: ignore
+            except:
+                return None
+        elif len(queryResult) == 0:
+            return None
+        else:
+            raise RuntimeError("Unusual amount of users found when logging in.")
+
+
+    def createBlock(
+        self,
+        identifier: str,
+        content: str
+    ) -> int:
+        
+        valueBlock: Block = Block(identifier, content)
+
+        self.modify_data("""
+            INSERT INTO data (identifier, value)
+            VALUES (%s, %s);
             """, 
-            [user.getUsername(), user.getUniqueid(), user.getEmail(), user.getAccessKey(), user.getPassword()])
+            [valueBlock.getIdentifier(), valueBlock.getValue()])
 
         return 1
 
 
-def getUsers(
-    username: Optional[str],
-    uniqueid: Optional[str],
-    email: Optional[str],
-    accessKey: Optional[str]
-) -> list[User]:
+    def getBlocks(
+        self,
+        identifier: str
+    ):
+        
+        queryResult = self.query_dict_data(("SELECT * FROM data WHERE identifier LIKE %s"), [(identifier + "%")])
+        
+        blocks: list[Block] = []
 
-    searchStatements: list[str] = []
-    data: list[str] = []
+        for result in queryResult:
+            block = Block(
+                identifier=result['identifier'], # type: ignore
+                value=result['value'] # type: ignore
+            )
+            blocks.append(block)
 
-    for key, value in {**locals()}.items():
-        if (type(value) == str):
-            searchStatements.append(f"{str(key)} = %s")
-            data.append(value)
-
-    searchStatement = " AND ".join(searchStatements)
-
-    queryResult = serviceInstance.query_dict_data(("SELECT * FROM users WHERE " + searchStatement), data)
-    
-    users: list[User] = []
-
-    for result in queryResult:
-        userEnv = utils.envs[str(utils.uniqueIdToMap(result['uniqueid'])['env'])] # type: ignore
-        user = User(
-            username=result['username'], # type: ignore
-            uniqueid=result['uniqueid'], # type: ignore
-            email=result['email'], # type: ignore
-            accessKey=result['accesskey'], # type: ignore
-            password=result['password'], # type: ignore
-            env=userEnv
-        )
-        users.append(user)
-
-    return users
+        return blocks
 
 
-def updateUser(
-    uniqueid: str,
-    valuesToUpdate: dict[str, str]
-):
-    
-    allowedValues: set[str] = {"email", "accessKey", "password"}
+    def updateBlock(
+        self,
+        identifier: str,
+        value: str
+    ):
+        
+        self.modify_data(("UPDATE data SET value=%s WHERE data.identifier = %s"), [value, identifier])
 
-    setStatements: list[str] = []
-
-    for key, value in valuesToUpdate.items():
-        if key not in allowedValues:
-            raise KeyError(f"Invalid key '{key}' in valuesToUpdate. Allowed keys are: {allowedValues}")
-        if key == "email" and not utils.validateEmail(value):
-            raise KeyError(f"Invalid email format for value: {value}")
-        setStatements.append(str(key + " = '" + value + "'"))
-
-    setStatement: str = ", ".join(setStatements)
-    
-    serviceInstance.modify_data(("UPDATE users SET " + setStatement + " WHERE users.uniqueID = %s"), [uniqueid])
-
-
-def login(
-    username: str,
-    password: str
-) -> str | None:
-    
-    queryResult = serviceInstance.query_dict_data(("SELECT * FROM users WHERE username=%s"), [username])
-
-    if len(queryResult) == 1:
-        try:
-            if utils.verifyHash(queryResult[0]["password"], password): # type: ignore
-                return queryResult[0]['accesskey'] # type: ignore
-        except:
-            return None
-    elif len(queryResult) == 0:
-        return None
-    else:
-        raise RuntimeError("Unusual amount of users found when logging in.")
-
-
-def createBlock(
-    identifier: str,
-    content: str
-) -> int:
-    
-    valueBlock: Block = Block(identifier, content)
-
-    serviceInstance.modify_data("""
-        INSERT INTO data (identifier, value)
-        VALUES (%s, %s);
-        """, 
-        [valueBlock.getIdentifier(), valueBlock.getValue()])
-
-    return 1
-
-
-def getBlocks(
-    identifier: str
-):
-    
-    queryResult = serviceInstance.query_dict_data(("SELECT * FROM data WHERE identifier LIKE %s"), [(identifier + "%")])
-    
-    blocks: list[Block] = []
-
-    for result in queryResult:
-        block = Block(
-            identifier=result['identifier'], # type: ignore
-            value=result['value'] # type: ignore
-        )
-        blocks.append(block)
-
-    return blocks
-
-
-def updateBlock(
-    identifier: str,
-    value: str
-):
-    
-    serviceInstance.modify_data(("UPDATE data SET value=%s WHERE data.identifier = %s"), [value, identifier])
-
-def deleteBlock(
-    identifier: str
-):
-    serviceInstance.modify_data(("DELETE FROM data WHERE data.identifier = %s"), [identifier])
+    def deleteBlock(
+        self,
+        identifier: str
+    ):
+        self.modify_data(("DELETE FROM data WHERE data.identifier = %s"), [identifier])
--- server.py
+++ server.py
@@ -50,6 +50,8 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+dbServiceInstance = dbService.DBService()
+
 @app.middleware("http")
 async def verify_request_credentials(request: Request, call_next): # type: ignore
     if request.url.path not in MIDDLEWARE_EXCLUSIONS and request.method != "OPTIONS":
@@ -59,7 +61,7 @@ async def verify_request_credentials(request: Request, call_next): # type: ignor
         accessKey = request.headers['RequesterAccessKey']
         if not accessKey or not username:
             return JSONResponse(status_code=401, content={"detail": "Authorization credentials required."})
-        if not dbService.verifyAccessKey(username, accessKey):
+        if not dbServiceInstance.verifyAccessKey(username, accessKey):
                         return JSONResponse(status_code=401, content={"detail": "Authorization credentials invalid."})
 
     try:
@@ -73,7 +75,7 @@ async def verify_request_credentials(request: Request, call_next): # type: ignor
 @app.post("/create_user", status_code=status.HTTP_204_NO_CONTENT)
 async def create_user( request: Annotated[CreateUserRequest, Query()] ):
     try:
-        dbService.createUser(request.username, request.email, request.password, request.test)
+        dbServiceInstance.createUser(request.username, request.email, request.password, request.test)
         return None
     except NonuniqueUsername as e:
         return JSONResponse(status_code=409, content={"detail": str(e)})
@@ -88,7 +90,7 @@ async def get_user( request: Annotated[GetUserRequest, Query()] ):
     if count < 1:
         raise RuntimeError("Must pass in at least ONE search parameter.")
 
-    users = dbService.getUsers(request.username, request.uniqueid, request.email, request.accessKey)
+    users = dbServiceInstance.getUsers(request.username, request.uniqueid, request.email, request.accessKey)
     if len(users) == 0:
         return None
 
@@ -101,9 +103,9 @@ async def get_user( request: Annotated[GetUserRequest, Query()] ):
 @app.patch("/update_user", status_code=status.HTTP_204_NO_CONTENT)
 async def update_user( request: Request, userRequest: Annotated[UpdateUserRequest, Query()] ):
     username = request.headers["RequesterUsername"]
-    uniqueid: str = dbService.getUsers(username, None, None, None)[0].getUniqueid()
+    uniqueid: str = dbServiceInstance.getUsers(username, None, None, None)[0].getUniqueid()
     try:
-        dbService.updateUser(uniqueid, json.loads(userRequest.newValuesJSON))
+        dbServiceInstance.updateUser(uniqueid, json.loads(userRequest.newValuesJSON))
     except KeyError as e:
         raise HTTPException(422, e.args[0])
 
@@ -112,7 +114,7 @@ async def update_user( request: Request, userRequest: Annotated[UpdateUserReques
 async def login( request: Annotated[LoginRequest, Query()] ):
     print(request.username + ", " + request.password)
     try:
-        accessKey: str | None = dbService.login(request.username, request.password)
+        accessKey: str | None = dbServiceInstance.login(request.username, request.password)
     except RuntimeError as e:
         raise HTTPException(500, e.args[0])
     
@@ -125,20 +127,20 @@ async def login( request: Annotated[LoginRequest, Query()] ):
 @app.post("/create_block", status_code=status.HTTP_204_NO_CONTENT)
 async def create_block( request: Request, userRequest: Annotated[CreateBlockRequest, Query()] ):
     username = request.headers["RequesterUsername"]
-    uniqueid: str = dbService.getUsers(username, None, None, None)[0].getUniqueid()
+    uniqueid: str = dbServiceInstance.getUsers(username, None, None, None)[0].getUniqueid()
     fullIdentifier = uniqueid + "." + userRequest.extendedIdentifier
     
-    dbService.createBlock(fullIdentifier, userRequest.value)
+    dbServiceInstance.createBlock(fullIdentifier, userRequest.value)
 
 
 @app.get("/get_blocks", response_model=GetBlocksResponse)
 async def get_blocks( request: Request, userRequest: Annotated[GetBlocksRequest, Query()] ):
     
     username = request.headers["RequesterUsername"]
-    uniqueid: str = dbService.getUsers(username, None, None, None)[0].getUniqueid()
+    uniqueid: str = dbServiceInstance.getUsers(username, None, None, None)[0].getUniqueid()
     fullIdentifier = uniqueid + "." + userRequest.extendedIdentifier
 
-    blocks = dbService.getBlocks(fullIdentifier)
+    blocks = dbServiceInstance.getBlocks(fullIdentifier)
     blocksNormalized: list[dict[str, str]] = []
     for block in blocks:
         blocksNormalized.append(block.__dict__)
@@ -149,22 +151,17 @@ async def get_blocks( request: Request, userRequest: Annotated[GetBlocksRequest,
 async def update_block( request: Request, userRequest: Annotated[UpdateBlockRequest, Query()] ):
 
     username = request.headers["RequesterUsername"]
-    uniqueid: str = dbService.getUsers(username, None, None, None)[0].getUniqueid()
+    uniqueid: str = dbServiceInstance.getUsers(username, None, None, None)[0].getUniqueid()
     fullIdentifier = uniqueid + "." + userRequest.extendedIdentifier
 
-    dbService.updateBlock(fullIdentifier, userRequest.value)
+    dbServiceInstance.updateBlock(fullIdentifier, userRequest.value)
 
 
 @app.post("/delete_block", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_block( request: Request, userRequest: Annotated[DeleteBlockRequest, Query()] ):
 
     username = request.headers["RequesterUsername"]
-    uniqueid: str = dbService.getUsers(username, None, None, None)[0].getUniqueid()
+    uniqueid: str = dbServiceInstance.getUsers(username, None, None, None)[0].getUniqueid()
     fullIdentifier = uniqueid + "." + userRequest.extendedIdentifier
 
-    dbService.deleteBlock(fullIdentifier)
-
-
-#if __name__ == "__main__":
-    #print(type(verifyAccessKey("aavidcomor", "1b4e9929685af5f7d0ed681e067c85b99063c7bc6a8f3dba97d72b542d65bac46fe839d888c04c4fab78310e35c3d01279b4ac4c799ad4bc6fdf195a6e01a424")))
-    #uvicorn.run("server:app", host="0.0.0.0", port=8000)
+    dbServiceInstance.deleteBlock(fullIdentifier)
